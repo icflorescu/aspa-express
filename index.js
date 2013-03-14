29e53f3ec9b9a51da2706c82792b71180494f904
Ionut-Cristian Florescu <ionut.florescu@gmail.com>
2013-03-14 17:00:12 +0200

    Added a few more comments

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ var fs = require('fs'),
     map;
 
 if (production) {
+    // Read the asset map synchronously and cache it
     map = JSON.parse(fs.readFileSync('aspa.json', 'utf8'));
 }
 
 /**
- * Utility method to get asset path.
+ * Utility method to get asset path based on the asset map (or simply pass-through in development mode).
  */
 exports.getAssetPath = function(asset) {
     if (production) {
@@ -17,13 +18,15 @@ exports.getAssetPath = function(asset) {
 };
 
 /**
- * Connect middleware to adjust response headers for gzipped assets.
+ * Connect-middleware to adjust response headers for gzipped assets.
  */
 exports.adjustResponseHeaders = function(req, res, next) {
     var path = req.path;
 
     if (path.slice(-3) === '.gz') {
         res.set('Content-Encoding', 'gzip');
+
+        // Add the appropriate headers for .css and .js files to avoid console warnings in some browsers
         if (path.slice(-7, -3) === '.css') {
             res.set('Content-Type', 'text/css');
         } else if (path.slice(-6, -3) === '.js') {
